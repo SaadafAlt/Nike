fdf14dca1f83b97965409c802551fc58c1df5f6a
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-09 15:47:46 -0400

    successfully add ShoeCard interactive images to hero

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,15 +1,28 @@
-const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
-  return (
-    <div className={`border-2 
-    rounded-xl
-      ${bigShoeImage === imgURL
-        ? 'border-coral-red'
-        : 'border-transparent'
-      } cursor-pointer max-sm:flex-1
-    `}>
-      {/* onClick={handleClick} */}
-    </div>
-  )
-}
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
 
-export default ShoeCard
+  return (
+    // In here we must determine if the current img is the currently selected shoe img,
+    // meaning we have to change the styles dynamically to either add/remove the border
+    // from the currently selected img.
+    <div
+      className={`border-2 
+    rounded-xl
+      ${
+        bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
+      } cursor-pointer max-sm:flex-1
+    `}
+      onClick={handleClick}
+    >
+      <div>
+        <img src={imgURL.thumbnail} />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
