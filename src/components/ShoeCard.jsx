6313e2df2ff8fd3c03176ae26e322410a4590b45
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-09 22:08:53 -0400

    WIP building ShoeCard interactivity

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -18,8 +18,13 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     `}
       onClick={handleClick}
     >
-      <div>
-        <img src={imgURL.thumbnail} />
+      <div className="flex justify-center items-center bg-card bg-center
+      sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img src={imgURL.thumbnail}
+        alt="shoe collection"
+        width={127}
+        height={103}
+        className="object-contain" />
       </div>
     </div>
   );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,10 @@ import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
@@ -45,13 +47,13 @@ const Hero = () => {
         className="object-contain relative z-10"
         />
 
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard 
                 imgURL={shoe} // images
-                changeBigShoeImage={() => {}}
-                bigShoeImg=""
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
