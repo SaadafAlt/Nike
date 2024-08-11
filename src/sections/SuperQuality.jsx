199244cfb84b3c91e0004bc30cef94c98f5ceb91
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-11 16:42:46 -0400

    update Button component params. to take in more styling, add SpecialOffer Component section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import { shoe8 } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
 const SuperQuality = () => {
   return (
@@ -24,7 +25,8 @@ const SuperQuality = () => {
             Our dedication to detail and excellence ensures your staisfaction
           </p>
           <div className="mt-11">
-            <Button label="View Details" />
+            <Button label="Shop Now"
+            iconURL={arrowRight} />
           </div>
         </div>
         {/* The div below justifies horizontally and vertically */}
