05993fe17f0bc551efad185b7447a874b02a019f
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-12 12:53:28 -0400

    remove border from hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center 
-      min-h-screen gap-10 max-container border-2 border-red-500 p-2"
+      min-h-screen gap-10 max-container p-2"
     >
       <div
         className="relative xl:w-2/5 flex flex-col justify-center items-start
